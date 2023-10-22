007b4950c3143c8d73bc7a298fc1c6165444e900
Joseph Lumpkin <nictus1938@gmail.com>
2023-10-22 17:20:53 -0400

    Create ProgressibleUnit.java
    Created the ProgressibleUnit interface.

---
+++ app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/units/ProgressibleUnit.java
@@ -0,0 +1,21 @@
+package com.ccsu.designpatterns.fall23.alieninvasionsim.units;
+
+/**
+ * An interface to detail all progress
+ * that a progressible unit might need to handle.
+ *
+ * @author Joseph Lumpkin
+ */
+public interface ProgressibleUnit {
+
+    /**
+     * Handle the progress associated with this unit.
+     *
+     * @param board Game board object in which the unit resides in.
+     *
+     * @author  Joseph Lumpkin
+     * @version 1.0
+     * @since 2023-22-10
+     */
+    public void progress(Board board);
+}

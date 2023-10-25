7dd0540c5b164cea710eef319e0916434ad8b1b7
caprav <vines07@gmail.com>
2023-10-25 15:58:15 -0400

    added Lifeforms packages and relevant Human, Alien, etc classes.

---
+++ app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Lifeforms/Saiyan.java
@@ -0,0 +1,14 @@
+package com.ccsu.designpatterns.fall23.alieninvasionsim.Lifeforms;
+/**
+ * A class to define the behavior of Saiyan Alien
+ * lifeforms within the simulation
+ *
+ * @author Vincent Capra
+ */
+public class Saiyan extends Alien{
+
+    @Override
+    protected void move() {
+
+    }
+}

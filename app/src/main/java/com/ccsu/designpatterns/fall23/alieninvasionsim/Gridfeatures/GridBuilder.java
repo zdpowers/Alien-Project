f8884416b27abfecf2e8e77b7005ef467cbe1e44
caprav <vines07@gmail.com>
2023-10-25 10:04:43 -0400

    changed some variables for better code readability. Added some additional functionality to GridBuilder class.

--- app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Gridfeatures/GridBuilder.java
+++ app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Gridfeatures/GridBuilder.java
@@ -1,14 +1,18 @@
 package com.ccsu.designpatterns.fall23.alieninvasionsim.Gridfeatures;
 
 import android.content.Context;
+import java.util.Random;
 /**
  * A class to build the simulation grid
  *
  * @author Vincent Capra
  */
 public class GridBuilder {
-    private int numberOfTilesInGrid = 0;
+    private int numberOfTilesInGrid = 0, gridSize;
+    private int randomXCoord, randomYCoord;
     private GridTile[][] gridLayout;
+    private Random randomNum = new Random();
+
 
     /**
      * This method initializes the grid layout based on a given
@@ -16,17 +20,17 @@ public class GridBuilder {
      * and other resources on the map
      *
      * @author Vincent Capra
-     * @param gridSize the number of tiles to make a square
+     * @param grid_Size the number of tiles to make a square
      *                 where length of side = gridSize
      */
-    public GridBuilder(int gridSize) {
-        gridLayout = new GridTile[gridSize][gridSize];
-
+    public GridBuilder(int grid_Size) {
+        gridLayout = new GridTile[grid_Size][grid_Size];
+        gridSize = grid_Size;
         // VPC - loop through the rows
-        for (int i = 1; i <= gridSize; i++) {
+        for (int i = 1; i <= grid_Size; i++) {
 
             //loop through the columns adding tiles and setting neighbors
-            for (int j = 1; j <= gridSize; j++) {
+            for (int j = 1; j <= grid_Size; j++) {
                 //VPC - create the grid tile for current location
                 gridLayout[i][j] = new GridTile(j, i);
                 numberOfTilesInGrid++; //this might not be necessary...?
@@ -48,6 +52,9 @@ public class GridBuilder {
         int currentNumOfWaterTiles = 0;
 
         while (currentNumOfWaterTiles <= maxNumOfWaterTiles) {
+            randomXCoord = randomNum.nextInt(gridSize)+1;
+            randomYCoord = randomNum.nextInt(gridSize)+1;;
+
 
             // VPC - need a collection here to easily figure out if a
             // tile is a already a water tile or not...
@@ -55,9 +62,17 @@ public class GridBuilder {
             // resource tiles in placeResourceTiles method too.
 
             // need to randomize seeds 1, 2, 3 and then start replacing water tiles
+
+            currentNumOfWaterTiles ++;
         }
     }
 
+    /**
+     * This method places connected water tiles in the map based on seed values
+     * until ~30% of the map is water
+     *
+     * @author Vincent Capra
+     */
     private void placeResourceTiles(){
 
     }

889bf821f157fb32af42718a843b9573b1079854
rhtmtb <rocky.trinh29@gmail.com>
2023-10-27 17:31:39 -0400

    Feat. LifeformIterator
    Not sure if this is where I should have put this... :) . This is the logic for the Lifeform iterator...not sure if it works/how to test it.

--- app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Gridfeatures/TerrainTile.java
+++ app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Gridfeatures/TerrainTile.java
@@ -1,5 +1,13 @@
 package com.ccsu.designpatterns.fall23.alieninvasionsim.Gridfeatures;
 
+import android.service.quicksettings.TileService;
+
+import com.ccsu.designpatterns.fall23.alieninvasionsim.Lifeforms.Lifeform;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * A class to build single terrain tiles. These are tiles that can be
  * occupied by a single country of humans or a single race of aliens
@@ -15,7 +23,68 @@ public class TerrainTile extends GridTile{
         //need to place the tile on the board
     }
 
-    /**
+    //not sure if this is implemented already
+/*    public class GameBoard {
+        private List<Lifeform> lifeforms; //store lifeforms on gameboard
+        private List<Tile> tiles; // store tiles on gameboard
+
+        //constructor
+        public GameBoard(List<Lifeform> lifeforms, List<Tile> tiles) {
+            this.lifeforms = lifeforms;
+            this.tiles = tiles;
+        }*/
+
+        //create and return a lifeform iterator
+        public Iterator<Lifeform> createLifeformIterator() {
+            return new LifeformIterator();
+        }
+
+        /**
+         * The createLifeformIterator is a method that returns an iterator for the Lifeform iterator
+         * The LifeformIterator is a private inner class that implements the iterator.
+         *
+         * @author Rocky Trinh
+         */
+
+        private class LifeformIterator implements Iterator<Lifeform> {
+            private int index = 0; //keep track of current position
+
+            //method to check if there are lifeforms to iterate through
+            @Override
+            public boolean hasNext() {
+                return index < lifeform.size();
+            }
+            //method to get the next lifeform and update index
+            @Override
+            public Lifeform next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return lifeform.get(index++);
+            }
+        }
+    }
+
+    public class Main {
+        public void main(String[] args) {
+            //Create lifeforms and tiles
+
+            List<Lifeform> lifeforms = new ArrayList<>();
+            List<Tiles> tiles = new ArrayList<>();
+
+            //populate lifeforms and tiles
+            GameBoard gameBoard = new GameBoard(lifeforms, tiles);
+
+            //iterate through the lifeforms
+            Iterator<Lifeform> LifeformIterator = gameBoard.createLifeformIterator();
+            while (lifeformIterator().hasNext()) {
+                Lifeform lifeform = lifeformIterator.next();
+                //lifeform actions can go here
+            }
+        }
+    }
+
+        /**
      * method to indicate if the tile is occupied
      *
      * @author Vincent Capra
@@ -51,5 +120,3 @@ public class TerrainTile extends GridTile{
 
         // put the count of occupants on the tile too
     }
-
-}

19e6383168a383010d7f1240a25f79386953aa23
rhtmtb <rocky.trinh29@gmail.com>
2023-10-29 10:58:23 -0400

    Feat. implemented Grid LifeformIterator
    added javadocs and lifeform iterator

--- app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Grid/Grid.java
+++ app/src/main/java/com/ccsu/designpatterns/fall23/alieninvasionsim/Grid/Grid.java
@@ -1,5 +1,31 @@
 package com.ccsu.designpatterns.fall23.alieninvasionsim.Grid;
 
+import com.ccsu.designpatterns.fall23.alieninvasionsim.Lifeforms.LifeForm;
+import java.util.ArrayList;
+/**
+ * A class to hold lifeforms and tile data
+ *
+ *
+ * @author Rocky Trinh
+ * @version 1.0
+ * @since 2023-29-10
+ */
 public class Grid {
-    // Template class to avoid compilation errors for now
+    /** Lifeforms are a list of all lifeforms in the simulation */
+    ArrayList<LifeForm> lifeforms = new ArrayList<>();
+    //ArrayList<Tiles> tiles = new ArrayList<>();
+
+    /**
+     * iterates through the arraylist of all
+     * lifeforms in the simulation and calls .progress().
+     *  @author Rocky Trinh
+     *  @version 1.0
+     *  @since 2023-29-10
+     */
+    public void ProgressLifeforms() {
+        for(LifeForm i : lifeforms){
+            //lifeform operations
+            i.progress(this);
+        }
+    }
 }
